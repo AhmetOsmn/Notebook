0de13d865c9259d975a9247f3a7de4937d3e1f85
Ahmet Osman Sezgin <44196434+AhmetOsmn@users.noreply.github.com>
2023-03-02 13:35:42 +0300

    unary ornegi eklendi

---
+++ gRPC/Examples/gRPC.Unary/gRPC.Unary.gRPCClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Net.Client;
+
+namespace gRPC.Unary.gRPCClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Title = "Client";
+
+            var channel = GrpcChannel.ForAddress("https://localhost:7010");
+            var messageClient = new Message.MessageClient(channel);
+
+            var response = await messageClient.SendMessageAsync(new MessageRequest
+            {
+                Name = "Ahmet",
+                Message = "Merhaba"
+            });
+
+            Console.WriteLine($"Döndürülen yanıt: {response.Message}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ gRPC/Examples/gRPC.Unary/gRPC.Unary.gRPCServer/Services/MessageService.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Core;
+
+namespace gRPC.Unary.gRPCServer.Services
+{
+    public class MessageService : Message.MessageBase
+    {
+        public async override Task<MessageResponse> SendMessage(MessageRequest request, ServerCallContext context)
+        {
+            Console.Title = "Server";
+            Console.WriteLine($"{request.Name}: {request.Message}");
+
+            return new MessageResponse { Message = "Mesaj alındı" };
+        }
+    }
+}
