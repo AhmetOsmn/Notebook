0cc541cb4d5a128019bd2cb980b1410e0740f9f0
Ahmet Osman Sezgin <44196434+AhmetOsmn@users.noreply.github.com>
2023-03-02 14:09:44 +0300

    client streaming ornegi eklendi

---
+++ gRPC/Examples/gRPC.ClientStreaming/gRPC.ClientStreaming.gRPCClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using gRPC.Unary.gRPCClient;
+using Grpc.Net.Client;
+
+namespace gRPC.ClientStreaming.gRPCClient
+{
+    internal static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:7100");
+            var messageClient = new Message.MessageClient(channel);
+
+            var request = messageClient.SendMessage();
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(1000);
+                await request.RequestStream.WriteAsync(new MessageRequest { Name = "ahmet " + i, Message = "merhaba " + i});
+            }
+
+            await request.RequestStream.CompleteAsync();
+
+            Console.WriteLine($"server'den gelen response: {(await request.ResponseAsync).Message}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ gRPC/Examples/gRPC.ClientStreaming/gRPC.ClientStreaming.gRPCServer/Services/MessageService.cs
@@ -0,0 +1,18 @@
+﻿using gRPC.Unary.gRPCServer;
+using Grpc.Core;
+
+namespace gRPC.ClientStreaming.gRPCServer.Services
+{
+    public class MessageService : Message.MessageBase
+    {
+        public override async Task<MessageResponse> SendMessage(IAsyncStreamReader<MessageRequest> requestStream, ServerCallContext context)
+        {
+            while (await requestStream.MoveNext(context.CancellationToken))
+            {
+                Console.WriteLine($"{requestStream.Current.Name}: {requestStream.Current.Message}");
+            }
+
+            return new MessageResponse { Message = "Servis verileri işledi" };
+        }
+    }
+}
