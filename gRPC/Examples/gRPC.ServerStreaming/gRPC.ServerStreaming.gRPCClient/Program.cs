c9363b428bbef294a3d3046950b385938127c430
Ahmet Osman Sezgin <44196434+AhmetOsmn@users.noreply.github.com>
2023-03-02 13:56:09 +0300

    server streaming ornegi eklendi

---
+++ gRPC/Examples/gRPC.ServerStreaming/gRPC.ServerStreaming.gRPCClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Net.Client;
+using gRPC.Unary.gRPCClient;
+
+namespace gRPC.ServerStreaming.gRPCClient
+{
+    internal static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:5002");
+            var messageClient = new Message.MessageClient(channel);
+
+            var response = messageClient.SendMessage(new MessageRequest
+            {
+                Name = "Ahmet",
+                Message = "Server streaming başladı:"
+            });
+
+            CancellationTokenSource cancellationTokenSource = new();
+
+            while (await response.ResponseStream.MoveNext(cancellationTokenSource.Token))
+            {
+                Console.WriteLine("Yanıt: " + response.ResponseStream.Current.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ gRPC/Examples/gRPC.ServerStreaming/gRPC.ServerStreaming.gRPCServer/Services/MessageService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+
+namespace gRPC.Unary.gRPCServer.Services
+{
+    public class MessageService : Message.MessageBase
+    {
+        public override async Task SendMessage(MessageRequest request, IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
+        {
+            Console.WriteLine($"{request.Name}: {request.Message}");
+
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(new MessageResponse { Message = "Merhaba" + i });
+            }
+        }
+    }
+}
