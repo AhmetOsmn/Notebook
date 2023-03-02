236468d2ff9ed0b041892aad31771c2aa7d09037
Ahmet Osman Sezgin <44196434+AhmetOsmn@users.noreply.github.com>
2023-03-02 14:31:16 +0300

    bidirectional streaming ornegi eklendi

---
+++ gRPC/Examples/gRPC.BidirectionalStreaming/gRPC.BidirectionalStreaming.gRPCClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using gRPC.Bidi.gRPCClient;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+namespace gRPC.BidirectionalStreaming.gRPCClient
+{
+    internal static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:7061");
+            var messageClient = new Message.MessageClient(channel);
+
+            var request = messageClient.SendMessage();
+
+            var task1 = Task.Run(async () =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    await Task.Delay(1000);
+                    await request.RequestStream.WriteAsync(new MessageRequest { Name = "Ahmet" +i, Message = "Merhaba " + i});
+                }
+
+                await request.RequestStream.CompleteAsync();
+            });
+
+            CancellationTokenSource cancellationTokenSource = new();
+
+            while (await request.ResponseStream.MoveNext(cancellationTokenSource.Token))
+            {
+                Console.WriteLine($"Server'dan client'a gelen mesaj: {request.ResponseStream.Current.Message}");
+            }
+
+            await task1;
+        }
+    }
+}
---
+++ gRPC/Examples/gRPC.BidirectionalStreaming/gRPC.BidirectionalStreaming.gRPCServer/Services/MessageService.cs
@@ -0,0 +1,27 @@
+﻿using gRPC.Bidi.gRPCServer;
+using Grpc.Core;
+
+namespace gRPC.BidirectionalStreaming.gRPCServer.Services
+{
+    public class MessageService : Message.MessageBase
+    {
+        public override async Task SendMessage(IAsyncStreamReader<MessageRequest> requestStream, IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
+        {
+            var task1 = Task.Run(async () =>
+            {
+                while (await requestStream.MoveNext(context.CancellationToken))
+                {
+                    Console.WriteLine($"Client'tan servera gelen mesaj: {requestStream.Current.Name} -> {requestStream.Current.Message}");
+                }
+            });
+
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(new MessageResponse { Message = "Mesaj alındı: " + i});
+            }
+
+            await task1;
+        }
+    }
+}
